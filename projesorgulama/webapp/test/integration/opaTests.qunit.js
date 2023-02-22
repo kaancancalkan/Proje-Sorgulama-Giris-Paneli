20c9073fb4764b285adf5782ad2ca54f9e5a66a3
Kaan Can Calkan <cancalkaniletisim@gmail.com>
2023-02-22 15:12:01 +0300

    Fİrst Push

---
+++ projesorgulama/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["girispaneli/projesorgulama/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ projesorgulama/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"girispaneli/projesorgulama/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
